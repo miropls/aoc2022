4a5effea03b5b6a9ddee29c4e0afffb401f8b50e
Miro Paintola <miropls@pop-os.localdomain>
2023-08-05 21:44:11 +0300

    init + day 1

---
+++ 01/go.mod
@@ -0,0 +1,3 @@
+module github.com/miropls/aoc2022/01
+
+go 1.20
---
+++ 01/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	listOfElves := []int{}
+	totalCalories := 0
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			listOfElves = append(listOfElves, totalCalories)
+			totalCalories = 0
+			scanner.Scan()
+		}
+
+		num, err := strconv.Atoi(scanner.Text())
+
+		if err != nil {
+			break
+		}
+
+		totalCalories += num
+	}
+
+	sort.SliceStable(listOfElves, func(i, j int) bool {
+		return listOfElves[i] > listOfElves[j]
+	})
+
+	fmt.Println(listOfElves[0])
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
